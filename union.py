b56ce5d0f39d92c34c2f022ea357841d4c522b7a
shikha2205 <165188043+shikha2205@users.noreply.github.com>
2024-10-26 20:57:13 +0530

    Add files via upload

---
+++ union.py
@@ -0,0 +1,10 @@
+E = {0, 2, 4, 6, 8}
+N = {1, 2, 3, 4, 5}
+union_result = E | N
+intersection_result = E & N
+difference_result = E - N
+symmetric_difference_result = E ^ N
+print("Union of E and N is", union_result)
+print("Intersection of E and N is", intersection_result)
+print("Difference of E and N is", difference_result)
+print("Symmetric difference of E and N is", symmetric_difference_result)
